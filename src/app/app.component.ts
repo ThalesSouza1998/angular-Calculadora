56ef732967202b213ad5c3e1514d8702495d8541
ThalesSouza1998 <108632249+ThalesSouza1998@users.noreply.github.com>
2022-11-01 22:56:15 -0300

    Atualização 01/11/2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,96 +6,110 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public primeiro: number = 0;
-  public segundo: number = 0;
+  public digitop: string = "";
+  public digitos: string = "";
+  public primeiro: number = -1;
+  public segundo: number = -1;
   public resultado: number = 0;
   public calcular: number = 0;
   
 
  
   um(){
-    if(this.primeiro<=0)
-    this.primeiro = 1
+    if(this.primeiro == -1)
+      this.digitop = this.digitop + "1"
     else(
-      this.segundo = 1
+      this.digitos = this.digitos + "1"
     )
   }
   dois(){
-    if(this.primeiro<=0)
-    this.primeiro = 2
-    else(
-      this.segundo = 2
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "2"
+  else
+    this.digitos = this.digitos + "2"
+  
   }
   tres(){
-    if(this.primeiro<=0)
-    this.primeiro = 3
-    else(
-      this.segundo = 3
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "3"
+  else(
+    this.digitos = this.digitos + "3"
+  )
   }
   quatro(){
-    if(this.primeiro<=0)
-    this.primeiro = 4
-    else(
-      this.segundo = 4
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "4"
+  else(
+    this.digitos = this.digitos + "4"
+  )
   }
   cinco(){
-    if(this.primeiro<=0)
-    this.primeiro = 5
-    else(
-      this.segundo = 5
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "5"
+  else(
+    this.digitos = this.digitos + "5"
+  )
   }
   seis(){
-    if(this.primeiro<=0)
-    this.primeiro = 6
-    else(
-      this.segundo = 6
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "6"
+  else(
+    this.digitos = this.digitos + "6"
+  )
   }
   sete(){
-    if(this.primeiro<=0)
-    this.primeiro = 7
-    else(
-      this.segundo = 7
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "7"
+  else(
+    this.digitos = this.digitos + "7"
+  )
   }
   oito(){
-    if(this.primeiro<=0)
-    this.primeiro = 8
-    else(
-      this.segundo = 8
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "8"
+  else(
+    this.digitos = this.digitos + "8"
+  )
   }
   nove(){
-    if(this.primeiro<=0)
-    this.primeiro = 9
-    else(
-      this.segundo = 9
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "9"
+  else(
+    this.digitos = this.digitos + "9"
+  )
   }
   zero(){
-    if(this.primeiro<=0)
-    this.primeiro = 0
-    else(
-      this.segundo = 0
-    )
+    if(this.primeiro == -1)
+    this.digitop = this.digitop + "0"
+  else(
+    this.digitos = this.digitos + "0"
+  )
   }
   somar(){
+    this.primeiro = parseFloat(this.digitop)
+    this.segundo = parseFloat(this.digitos)
     this.resultado = this.primeiro + this.segundo
   }
   limpar(){
     this.resultado = 0
+    this.primeiro = -1
+    this.segundo = -1
+    this.digitop = ""
+    this.digitos = ""
   }
   subtrair(){
+    this.primeiro = parseFloat(this.digitop)
+    this.segundo = parseFloat(this.digitos)
     this.resultado = this.primeiro - this.segundo
   }
   divisao(){
+    this.primeiro = parseFloat(this.digitop)
+    this.segundo = parseFloat(this.digitos)
     this.resultado = this.primeiro / this.segundo
   }
   multiplicacao(){
+    this.primeiro = parseFloat(this.digitop)
+    this.segundo = parseFloat(this.digitos)
     this.resultado = this.primeiro * this.segundo
   }
   
